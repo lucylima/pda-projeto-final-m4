e28161c195b65de15aab39266412049bc61a7d65
brenddamachado <brenda16machado.bm@gmail.com>
2024-08-03 18:19:55 -0300

    route Teacher

---
+++ src/routes/Teacher.routes.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { createTeacher, getTeacher, updateTeacher, deleteTeacher } from '../controller/Teacher.controller.js';
+
+const TeacherRouter = Router();
+
+
+TeacherRouter.post('/cadastrar/professor', createTeacher);
+TeacherRouter.get('/professores', getTeacher);
+TeacherRouter.put('/atualizar/professor/:id', updateTeacher);
+TeacherRouter.delete('/deletar/professor/:id', deleteTeacher);
+
+export default TeacherRouter;
