1b2e9ad51ef0350448040f92e717b2f3e77b9cba
brenddamachado <brenda16machado.bm@gmail.com>
2024-08-03 13:37:04 -0300

    model e criação do controller

---
+++ src/model/Teacher.model.js
@@ -0,0 +1,26 @@
+import { DataTypes } from 'sequelize'
+import { database } from '../database/db.js'
+
+const Teacher = database.define('Teacher', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cpf: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  birthDate:{
+    type: DataTypes.DATEONLY,
+      allowNull: false
+    
+  }
+})
+
+export {Teacher}
