8633d4fb77ead4fd08a63b54a45344e63ccd9cd9
Junior Reis <gilmaikjunior@gmail.com>
2024-08-02 22:27:53 -0300

    add routerStudent, modelStudente and controllerStudent

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { tryConnectSequelize } from './database/db.js'
 import { userRouter } from './routes/User.routes.js'
 import { jobRouter } from './routes/job.routes.js'
+import { studentRouter } from './routes/Student.routes.js'
 
 const app = express()
 const port = process.env.PORT
@@ -9,6 +10,7 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 app.use(jobRouter)
+app.use(studentRouter)
 
 app.listen(port, () => {
   console.log(`servidor rodando na porta ${port}`)
