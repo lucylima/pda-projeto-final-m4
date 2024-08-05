6da27b17ffefcee57a86a57eb603ebe0d6203deb
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-08-05 08:55:01 -0300

    update: rotas admin renomeadas

--- src/routes/Admin.routes.js
+++ src/routes/Admin.routes.js
@@ -12,9 +12,9 @@ const adminRouter = Router()
 
 adminRouter.get('/admin', readAdmin)
 adminRouter.get("/admin/:id", readAdminParam)
-adminRouter.get("/admin/reveal/:access/:cpf", revealAdminToken)
-adminRouter.post('/admin/create', createAdmin)
-adminRouter.put('/admin/:token', editAdmin)
-adminRouter.delete('/admin/:cpf/:token', deleteAdmin)
+adminRouter.get("/admin/revelar-token/:access/:cpf", revealAdminToken)
+adminRouter.post('/admin/cadastrar', createAdmin)
+adminRouter.put('/admin/editar/:token', editAdmin)
+adminRouter.delete('/admin/remover/:cpf/:token', deleteAdmin)
 
 export { adminRouter }
