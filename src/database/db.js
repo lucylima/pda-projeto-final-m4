d5447e39b689e22d7c6a153c4f9faccf9952ce52
Lucy Lima <lucyatmosphere@gmail.com>
2024-08-13 22:15:32 -0300

    fix: rotas renomeadas

--- src/database/db.js
+++ src/database/db.js
@@ -2,13 +2,13 @@ import { Sequelize } from 'sequelize'
 import 'dotenv/config'
 
 const database = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: 'postgres'
 })
 
 const tryConnectSequelize = async () => {
   try {
     await database.authenticate()
-    await database.sync({ logging: false})
+    await database.sync({ logging: false })
     return console.log('Conexão bem-sucedida')
   } catch (error) {
     return console.error('Erro de conexão: ', error)
